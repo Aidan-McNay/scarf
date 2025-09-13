0d9da4125eb7c7ded8cd44973ae18f219683d169
Aidan McNay <aidanmcnay@gmail.com>
2025-09-13 14:36:59 -0400

    Started port to winnow

--- scarf-parser/src/parser/declarations/declaration_data_types/mod.rs
+++
@@ -1,9 +0,0 @@
-// =======================================================================
-// mod.rs
-// =======================================================================
-// Parsing for 1800-2023 A.2.2
-
-pub mod net_and_variable_types;
-pub mod strengths;
-pub use net_and_variable_types::*;
-pub use strengths::*;
