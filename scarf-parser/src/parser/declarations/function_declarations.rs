52633a59bd6297032825d6881f2d74e4dfd492df
Aidan <acm289@cornell.edu>
2025-10-17 19:23:59 -0700

    Implement function declarations

--- scarf-parser/src/parser/declarations/function_declarations.rs
+++ scarf-parser/src/parser/declarations/function_declarations.rs
@@ -7,10 +7,194 @@ use crate::*;
 use scarf_syntax::*;
 use winnow::ModalResult;
 use winnow::Parser;
-use winnow::combinator::fail;
+use winnow::combinator::{alt, opt, repeat};
+use winnow::token::any;
+
+pub fn function_data_type_or_implicit_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<FunctionDataTypeOrImplicit<'s>, VerboseError<'s>> {
+    alt((
+        data_type_or_void_parser
+            .map(|a| FunctionDataTypeOrImplicit::DataType(Box::new(a))),
+        implicit_data_type_parser
+            .map(|a| FunctionDataTypeOrImplicit::Implicit(Box::new(a))),
+    ))
+    .parse_next(input)
+}
 
 pub fn function_declaration_parser<'s>(
     input: &mut Tokens<'s>,
 ) -> ModalResult<FunctionDeclaration<'s>, VerboseError<'s>> {
-    fail.parse_next(input)
+    (
+        token(Token::Function),
+        opt_dynamic_override_specifiers_parser,
+        opt(lifetime_parser),
+        function_body_declaration_parser,
+    )
+        .map(|(a, b, c, d)| FunctionDeclaration(a, b, c, d))
+        .parse_next(input)
+}
+
+pub fn function_body_declaration_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<FunctionBodyDeclaration<'s>, VerboseError<'s>> {
+    let _tf_parser = (
+        function_data_type_or_implicit_parser,
+        interface_identifier_or_class_scope_parser,
+        function_identifier_parser,
+        token(Token::SColon),
+        repeat(0.., tf_item_declaration_parser),
+        repeat(0.., function_statement_or_null_parser),
+        token(Token::Endfunction),
+        opt((token(Token::Colon), function_identifier_parser)),
+    )
+        .map(|(a, b, c, d, e, f, g, h)| {
+            FunctionBodyDeclaration::Tf(Box::new((a, b, c, d, e, f, g, h)))
+        });
+    let _block_parser = (
+        function_data_type_or_implicit_parser,
+        interface_identifier_or_class_scope_parser,
+        function_identifier_parser,
+        token(Token::Paren),
+        opt(tf_port_list_parser),
+        token(Token::EParen),
+        token(Token::SColon),
+        repeat(0.., block_item_declaration_parser),
+        repeat(0.., function_statement_or_null_parser),
+        token(Token::Endfunction),
+        opt((token(Token::Colon), function_identifier_parser)),
+    )
+        .map(|(a, b, c, d, e, f, g, h, i, j, k)| {
+            FunctionBodyDeclaration::Block(Box::new((
+                a, b, c, d, e, f, g, h, i, j, k,
+            )))
+        });
+    alt((_tf_parser, _block_parser)).parse_next(input)
+}
+
+pub fn function_prototype_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<FunctionPrototype<'s>, VerboseError<'s>> {
+    (
+        token(Token::Function),
+        opt_dynamic_override_specifiers_parser,
+        data_type_or_void_parser,
+        function_identifier_parser,
+        opt((
+            token(Token::Paren),
+            opt(tf_port_list_parser),
+            token(Token::EParen),
+        )),
+    )
+        .map(|(a, b, c, d, e)| FunctionPrototype(a, b, c, d, e))
+        .parse_next(input)
+}
+
+pub fn dpi_import_export_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<DpiImportExport<'s>, VerboseError<'s>> {
+    let _function_import_parser = (
+        token(Token::Import),
+        dpi_spec_string_parser,
+        opt(dpi_function_import_property_parser),
+        opt((c_identifier_parser, token(Token::Eq))),
+        dpi_function_proto_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d, e, f)| {
+            DpiImportExport::FunctionImport(Box::new((a, b, c, d, e, f)))
+        });
+    let _task_import_parser = (
+        token(Token::Import),
+        dpi_spec_string_parser,
+        opt(dpi_task_import_property_parser),
+        opt((c_identifier_parser, token(Token::Eq))),
+        dpi_task_proto_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d, e, f)| {
+            DpiImportExport::TaskImport(Box::new((a, b, c, d, e, f)))
+        });
+    let _function_export_parser = (
+        token(Token::Export),
+        dpi_spec_string_parser,
+        opt((c_identifier_parser, token(Token::Eq))),
+        token(Token::Function),
+        function_identifier_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d, e, f)| {
+            DpiImportExport::FunctionExport(Box::new((a, b, c, d, e, f)))
+        });
+    let _task_export_parser = (
+        token(Token::Export),
+        dpi_spec_string_parser,
+        opt((c_identifier_parser, token(Token::Eq))),
+        token(Token::Task),
+        task_identifier_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d, e, f)| {
+            DpiImportExport::TaskExport(Box::new((a, b, c, d, e, f)))
+        });
+    alt((
+        _function_import_parser,
+        _task_import_parser,
+        _function_export_parser,
+        _task_export_parser,
+    ))
+    .parse_next(input)
+}
+
+pub fn dpi_spec_string_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<DpiSpecString<'s>, VerboseError<'s>> {
+    any.verify_map(|s: &'s SpannedToken<'s>| match s.0 {
+        Token::SimpleIdentifier("DPI-C") => {
+            Some(DpiSpecString::DpiC(Metadata {
+                span: s.1.clone(),
+                extra_nodes: vec![],
+            }))
+        }
+        Token::SimpleIdentifier("DPI") => Some(DpiSpecString::Dpi(Metadata {
+            span: s.1.clone(),
+            extra_nodes: vec![],
+        })),
+        _ => None,
+    })
+    .parse_next(input)
+}
+
+pub fn dpi_function_import_property_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<DpiFunctionImportProperty<'s>, VerboseError<'s>> {
+    alt((
+        token(Token::Context).map(|a| DpiFunctionImportProperty::Context(a)),
+        token(Token::Pure).map(|a| DpiFunctionImportProperty::Pure(a)),
+    ))
+    .parse_next(input)
+}
+
+pub fn dpi_task_import_property_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<DpiTaskImportProperty<'s>, VerboseError<'s>> {
+    token(Token::Context)
+        .map(|a| DpiTaskImportProperty(a))
+        .parse_next(input)
+}
+
+pub fn dpi_function_proto_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<DpiFunctionProto<'s>, VerboseError<'s>> {
+    function_prototype_parser
+        .map(|a| DpiFunctionProto(a))
+        .parse_next(input)
+}
+
+pub fn dpi_task_proto_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<DpiTaskProto<'s>, VerboseError<'s>> {
+    task_prototype_parser
+        .map(|a| DpiTaskProto(a))
+        .parse_next(input)
 }
--- scarf-syntax/src/declarations/function_declarations.rs
+++ scarf-syntax/src/declarations/function_declarations.rs
@@ -3,6 +3,147 @@
 // =======================================================================
 // AST Nodes from 1800-2023 A.2.6
 
-// use crate::*;
+use crate::*;
 
-pub type FunctionDeclaration<'a> = ();
+#[derive(Clone, Debug, PartialEq)]
+pub enum FunctionDataTypeOrImplicit<'a> {
+    DataType(Box<DataTypeOrVoid<'a>>),
+    Implicit(Box<ImplicitDataType<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FunctionDeclaration<'a>(
+    pub Metadata<'a>, // function
+    pub Option<DynamicOverrideSpecifiers<'a>>,
+    pub Option<Lifetime<'a>>,
+    pub FunctionBodyDeclaration<'a>,
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum FunctionBodyDeclaration<'a> {
+    Tf(
+        Box<(
+            FunctionDataTypeOrImplicit<'a>,
+            InterfaceIdentifierOrClassScope<'a>,
+            FunctionIdentifier<'a>,
+            Metadata<'a>, // ;
+            Vec<TfItemDeclaration<'a>>,
+            Vec<FunctionStatementOrNull<'a>>,
+            Metadata<'a>, // endfunction
+            Option<(
+                Metadata<'a>, // :
+                FunctionIdentifier<'a>,
+            )>,
+        )>,
+    ),
+    Block(
+        Box<(
+            FunctionDataTypeOrImplicit<'a>,
+            InterfaceIdentifierOrClassScope<'a>,
+            FunctionIdentifier<'a>,
+            Metadata<'a>, // (
+            Option<TfPortList<'a>>,
+            Metadata<'a>, // )
+            Metadata<'a>, // ;
+            Vec<BlockItemDeclaration<'a>>,
+            Vec<FunctionStatementOrNull<'a>>,
+            Metadata<'a>, // endfunction
+            Option<(
+                Metadata<'a>, // :
+                FunctionIdentifier<'a>,
+            )>,
+        )>,
+    ),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct FunctionPrototype<'a>(
+    pub Metadata<'a>, // function
+    pub Option<DynamicOverrideSpecifiers<'a>>,
+    pub DataTypeOrVoid<'a>,
+    pub FunctionIdentifier<'a>,
+    pub  Option<(
+        Metadata<'a>, // (
+        Option<TfPortList<'a>>,
+        Metadata<'a>, // )
+    )>,
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum DpiImportExport<'a> {
+    FunctionImport(
+        Box<(
+            Metadata<'a>, // import
+            DpiSpecString<'a>,
+            Option<DpiFunctionImportProperty<'a>>,
+            Option<(
+                CIdentifier<'a>,
+                Metadata<'a>, // =
+            )>,
+            DpiFunctionProto<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    TaskImport(
+        Box<(
+            Metadata<'a>, // import
+            DpiSpecString<'a>,
+            Option<DpiTaskImportProperty<'a>>,
+            Option<(
+                CIdentifier<'a>,
+                Metadata<'a>, // =
+            )>,
+            DpiTaskProto<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    FunctionExport(
+        Box<(
+            Metadata<'a>, // export
+            DpiSpecString<'a>,
+            Option<(
+                CIdentifier<'a>,
+                Metadata<'a>, // =
+            )>,
+            Metadata<'a>, // function
+            FunctionIdentifier<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    TaskExport(
+        Box<(
+            Metadata<'a>, // export
+            DpiSpecString<'a>,
+            Option<(
+                CIdentifier<'a>,
+                Metadata<'a>, // =
+            )>,
+            Metadata<'a>, // task
+            TaskIdentifier<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum DpiSpecString<'a> {
+    DpiC(Metadata<'a>),
+    Dpi(Metadata<'a>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum DpiFunctionImportProperty<'a> {
+    Context(Metadata<'a>),
+    Pure(Metadata<'a>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DpiTaskImportProperty<'a>(
+    pub Metadata<'a>, // context
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DpiFunctionProto<'a>(pub FunctionPrototype<'a>);
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct DpiTaskProto<'a>(pub TaskPrototype<'a>);
