f8027dd739821d092809655a09507523cb3fbc0b
Aidan <acm289@cornell.edu>
2025-10-27 19:24:24 -0700

    Implement class items

--- scarf-parser/src/parser/source_text/class_items.rs
+++ scarf-parser/src/parser/source_text/class_items.rs
@@ -6,34 +6,328 @@
 use crate::*;
 use scarf_syntax::*;
 use winnow::ModalResult;
-use winnow::combinator::fail;
+use winnow::combinator::{alt, opt};
+
+enum ClassItemBody<'a> {
+    Property(ClassProperty<'a>),
+    Method(ClassMethod<'a>),
+    Constraint(ClassConstraint<'a>),
+    Declaration(ClassDeclaration<'a>),
+    InterfaceDeclaration(InterfaceClassDeclaration<'a>),
+    Covergroup(CovergroupDeclaration<'a>),
+}
 
 pub fn class_item_parser<'s>(
     input: &mut Tokens<'s>,
-) -> ModalResult<ClassItem, VerboseError<'s>> {
-    fail.parse_next(input)
+) -> ModalResult<ClassItem<'s>, VerboseError<'s>> {
+    let _class_item_body_parser = alt((
+        class_property_parser.map(|a| ClassItemBody::Property(a)),
+        class_method_parser.map(|a| ClassItemBody::Method(a)),
+        class_constraint_parser.map(|a| ClassItemBody::Constraint(a)),
+        class_declaration_parser.map(|a| ClassItemBody::Declaration(a)),
+        interface_class_declaration_parser
+            .map(|a| ClassItemBody::InterfaceDeclaration(a)),
+        covergroup_declaration_parser.map(|a| ClassItemBody::Covergroup(a)),
+    ));
+    let _basic_class_item_parser = (
+        attribute_instance_vec_parser,
+        _class_item_body_parser,
+    )
+        .map(|(a, b)| match b {
+            ClassItemBody::Property(c) => ClassItem::Property(Box::new((a, c))),
+            ClassItemBody::Method(c) => ClassItem::Method(Box::new((a, c))),
+            ClassItemBody::Constraint(c) => {
+                ClassItem::Constraint(Box::new((a, c)))
+            }
+            ClassItemBody::Declaration(c) => {
+                ClassItem::Declaration(Box::new((a, c)))
+            }
+            ClassItemBody::InterfaceDeclaration(c) => {
+                ClassItem::InterfaceDeclaration(Box::new((a, c)))
+            }
+            ClassItemBody::Covergroup(c) => {
+                ClassItem::Covergroup(Box::new((a, c)))
+            }
+        });
+    let _local_parameter_parser =
+        (local_parameter_declaration_parser, token(Token::SColon))
+            .map(|(a, b)| ClassItem::LocalParameter(Box::new((a, b))));
+    let _parameter_parser =
+        (parameter_declaration_parser, token(Token::SColon))
+            .map(|(a, b)| ClassItem::Parameter(Box::new((a, b))));
+    let _null_parser =
+        token(Token::SColon).map(|a| ClassItem::Null(Box::new(a)));
+    alt((
+        _basic_class_item_parser,
+        _local_parameter_parser,
+        _parameter_parser,
+        _null_parser,
+    ))
+    .parse_next(input)
+}
+
+pub fn class_property_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassProperty<'s>, VerboseError<'s>> {
+    let _data_parser = (
+        repeat_strict( property_qualifier_parser),
+        data_declaration_parser,
+    )
+        .map(|(a, b)| ClassProperty::Data(Box::new((a, b))));
+    let _const_parser = (
+        token(Token::Const),
+        repeat_strict( class_item_qualifier_parser),
+        data_type_parser,
+        const_identifier_parser,
+        opt((token(Token::Eq), constant_expression_parser)),
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d, e, f)| {
+            ClassProperty::Const(Box::new((a, b, c, d, e, f)))
+        });
+    alt((_const_parser, _data_parser)).parse_next(input)
+}
+
+pub fn class_method_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassMethod<'s>, VerboseError<'s>> {
+    let _task_parser = (
+        repeat_strict( method_qualifier_parser),
+        task_declaration_parser,
+    )
+        .map(|(a, b)| ClassMethod::Task(Box::new((a, b))));
+    let _function_parser = (
+        repeat_strict( method_qualifier_parser),
+        function_declaration_parser,
+    )
+        .map(|(a, b)| ClassMethod::Function(Box::new((a, b))));
+    let _pure_virtual_method_parser = (
+        token(Token::Pure),
+        token(Token::Virtual),
+        repeat_strict( class_item_qualifier_parser),
+        method_prototype_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d, e)| {
+            ClassMethod::PureVirtualMethod(Box::new((a, b, c, d, e)))
+        });
+    let _extern_method_parser = (
+        token(Token::Extern),
+        repeat_strict( method_qualifier_parser),
+        method_prototype_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d)| ClassMethod::ExternMethod(Box::new((a, b, c, d))));
+    let _constructor_declaration_parser = (
+        repeat_strict( method_qualifier_parser),
+        class_constructor_declaration_parser,
+    )
+        .map(|(a, b)| ClassMethod::ConstructorDeclaration(Box::new((a, b))));
+    let _constructor_prototype_parser = (
+        token(Token::Extern),
+        repeat_strict( method_qualifier_parser),
+        class_constructor_prototype_parser,
+    )
+        .map(|(a, b, c)| {
+            ClassMethod::ConstructorPrototype(Box::new((a, b, c)))
+        });
+    alt((
+        _task_parser,
+        _pure_virtual_method_parser,
+        _extern_method_parser,
+        _constructor_declaration_parser,
+        _constructor_prototype_parser,
+        _function_parser,
+    ))
+    .parse_next(input)
+}
+
+pub fn list_of_arguments_or_default_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ListOfArgumentsOrDefault<'s>, VerboseError<'s>> {
+    alt((
+        list_of_arguments_parser
+            .map(|a| ListOfArgumentsOrDefault::ListOfArguments(Box::new(a))),
+        token(Token::Default)
+            .map(|a| ListOfArgumentsOrDefault::Default(Box::new(a))),
+    ))
+    .parse_next(input)
 }
 
 pub fn class_constructor_declaration_parser<'s>(
     input: &mut Tokens<'s>,
 ) -> ModalResult<ClassConstructorDeclaration<'s>, VerboseError<'s>> {
-    fail.parse_next(input)
+    (
+        token(Token::Function),
+        opt(class_scope_parser),
+        token(Token::New),
+        opt((
+            token(Token::Paren),
+            opt(class_constructor_arg_list_parser),
+            token(Token::EParen),
+        )),
+        token(Token::SColon),
+        repeat_strict( block_item_declaration_parser),
+        opt((
+            token(Token::Super),
+            token(Token::Period),
+            token(Token::New),
+            opt((
+                token(Token::Paren),
+                opt(list_of_arguments_or_default_parser),
+                token(Token::EParen),
+            )),
+            token(Token::SColon),
+        )),
+        repeat_strict( function_statement_or_null_parser),
+        token(Token::Endfunction),
+        opt((token(Token::Colon), token(Token::New))),
+    )
+        .map(|(a, b, c, d, e, f, g, h, i, j)| {
+            ClassConstructorDeclaration(a, b, c, d, e, f, g, h, i, j)
+        })
+        .parse_next(input)
+}
+
+pub fn class_constructor_prototype_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassConstructorPrototype<'s>, VerboseError<'s>> {
+    (
+        token(Token::Function),
+        token(Token::New),
+        opt((
+            token(Token::Paren),
+            opt(class_constructor_arg_list_parser),
+            token(Token::EParen),
+        )),
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d)| ClassConstructorPrototype(a, b, c, d))
+        .parse_next(input)
+}
+
+pub fn class_constructor_arg_list_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassConstructorArgList<'s>, VerboseError<'s>> {
+    (
+        class_constructor_arg_parser,
+        repeat_strict( (token(Token::Comma), class_constructor_arg_parser)),
+    )
+        .map(|(a, b)| ClassConstructorArgList(a, b))
+        .parse_next(input)
+}
+
+pub fn class_constructor_arg_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassConstructorArg<'s>, VerboseError<'s>> {
+    alt((
+        tf_port_item_parser.map(|a| ClassConstructorArg::TfPort(Box::new(a))),
+        token(Token::Default)
+            .map(|a| ClassConstructorArg::Default(Box::new(a))),
+    ))
+    .parse_next(input)
 }
 
 pub fn interface_class_item_parser<'s>(
     input: &mut Tokens<'s>,
-) -> ModalResult<InterfaceClassItem, VerboseError<'s>> {
-    fail.parse_next(input)
+) -> ModalResult<InterfaceClassItem<'s>, VerboseError<'s>> {
+    let _type_parser =
+        type_declaration_parser.map(|a| InterfaceClassItem::Type(Box::new(a)));
+    let _interface_class_parser =
+        (attribute_instance_vec_parser, interface_class_method_parser)
+            .map(|(a, b)| InterfaceClassItem::InterfaceClass(Box::new((a, b))));
+    let _local_parameter_parser =
+        (local_parameter_declaration_parser, token(Token::SColon))
+            .map(|(a, b)| InterfaceClassItem::LocalParameter(Box::new((a, b))));
+    let _parameter_parser =
+        (parameter_declaration_parser, token(Token::SColon))
+            .map(|(a, b)| InterfaceClassItem::Parameter(Box::new((a, b))));
+    let _null_parser =
+        token(Token::SColon).map(|a| InterfaceClassItem::Null(Box::new(a)));
+    alt((
+        _type_parser,
+        _interface_class_parser,
+        _local_parameter_parser,
+        _parameter_parser,
+        _null_parser,
+    ))
+    .parse_next(input)
 }
 
-pub fn method_prototype_parser<'s>(
+pub fn interface_class_method_parser<'s>(
     input: &mut Tokens<'s>,
-) -> ModalResult<MethodPrototype<'s>, VerboseError<'s>> {
-    fail.parse_next(input)
+) -> ModalResult<InterfaceClassMethod<'s>, VerboseError<'s>> {
+    (
+        token(Token::Pure),
+        token(Token::Virtual),
+        method_prototype_parser,
+        token(Token::SColon),
+    )
+        .map(|(a, b, c, d)| InterfaceClassMethod(a, b, c, d))
+        .parse_next(input)
+}
+
+pub fn class_constraint_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassConstraint<'s>, VerboseError<'s>> {
+    alt((
+        constraint_prototype_parser
+            .map(|a| ClassConstraint::Prototype(Box::new(a))),
+        constraint_declaration_parser
+            .map(|a| ClassConstraint::Declaration(Box::new(a))),
+    ))
+    .parse_next(input)
+}
+
+pub fn class_item_qualifier_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<ClassItemQualifier<'s>, VerboseError<'s>> {
+    alt((
+        token(Token::Static).map(|a| ClassItemQualifier::Static(a)),
+        token(Token::Protected).map(|a| ClassItemQualifier::Protected(a)),
+        token(Token::Local).map(|a| ClassItemQualifier::Local(a)),
+    ))
+    .parse_next(input)
+}
+
+pub fn property_qualifier_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<PropertyQualifier<'s>, VerboseError<'s>> {
+    alt((
+        random_qualifier_parser.map(|a| PropertyQualifier::Random(Box::new(a))),
+        class_item_qualifier_parser
+            .map(|a| PropertyQualifier::ClassItem(Box::new(a))),
+    ))
+    .parse_next(input)
 }
 
 pub fn random_qualifier_parser<'s>(
     input: &mut Tokens<'s>,
 ) -> ModalResult<RandomQualifier<'s>, VerboseError<'s>> {
-    fail.parse_next(input)
+    alt((
+        token(Token::Rand).map(|a| RandomQualifier::Rand(a)),
+        token(Token::Randc).map(|a| RandomQualifier::Randc(a)),
+    ))
+    .parse_next(input)
+}
+
+pub fn method_qualifier_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<MethodQualifier<'s>, VerboseError<'s>> {
+    let _pure_virtual_parser = (opt(token(Token::Pure)), token(Token::Virtual))
+        .map(|(a, b)| MethodQualifier::PureVirtual(Box::new((a, b))));
+    let _class_item_parser = class_item_qualifier_parser
+        .map(|a| MethodQualifier::ClassItem(Box::new(a)));
+    alt((_pure_virtual_parser, _class_item_parser)).parse_next(input)
+}
+
+pub fn method_prototype_parser<'s>(
+    input: &mut Tokens<'s>,
+) -> ModalResult<MethodPrototype<'s>, VerboseError<'s>> {
+    alt((
+        task_prototype_parser.map(|a| MethodPrototype::Task(Box::new(a))),
+        function_prototype_parser
+            .map(|a| MethodPrototype::Function(Box::new(a))),
+    ))
+    .parse_next(input)
 }
--- scarf-syntax/src/source_text/class_items.rs
+++ scarf-syntax/src/source_text/class_items.rs
@@ -3,8 +3,213 @@
 // =======================================================================
 // AST Nodes from 1800-2023 A.1.9
 
-pub type ClassItem = ();
-pub type ClassConstructorDeclaration<'a> = ();
-pub type InterfaceClassItem = ();
-pub type MethodPrototype<'a> = ();
-pub type RandomQualifier<'a> = ();
+use crate::*;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ClassItem<'a> {
+    Property(Box<(Vec<AttributeInstance<'a>>, ClassProperty<'a>)>),
+    Method(Box<(Vec<AttributeInstance<'a>>, ClassMethod<'a>)>),
+    Constraint(Box<(Vec<AttributeInstance<'a>>, ClassConstraint<'a>)>),
+    Declaration(Box<(Vec<AttributeInstance<'a>>, ClassDeclaration<'a>)>),
+    InterfaceDeclaration(
+        Box<(Vec<AttributeInstance<'a>>, InterfaceClassDeclaration<'a>)>,
+    ),
+    Covergroup(Box<(Vec<AttributeInstance<'a>>, CovergroupDeclaration<'a>)>),
+    LocalParameter(
+        Box<(
+            LocalParameterDeclaration<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    Parameter(
+        Box<(
+            ParameterDeclaration<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    Null(Box<Metadata<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ClassProperty<'a> {
+    Data(Box<(Vec<PropertyQualifier<'a>>, DataDeclaration<'a>)>),
+    Const(
+        Box<(
+            Metadata<'a>, // const
+            Vec<ClassItemQualifier<'a>>,
+            DataType<'a>,
+            ConstIdentifier<'a>,
+            Option<(
+                Metadata<'a>, // =
+                ConstantExpression<'a>,
+            )>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ClassMethod<'a> {
+    Task(Box<(Vec<MethodQualifier<'a>>, TaskDeclaration<'a>)>),
+    Function(Box<(Vec<MethodQualifier<'a>>, FunctionDeclaration<'a>)>),
+    PureVirtualMethod(
+        Box<(
+            Metadata<'a>, // pure
+            Metadata<'a>, // virtual
+            Vec<ClassItemQualifier<'a>>,
+            MethodPrototype<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    ExternMethod(
+        Box<(
+            Metadata<'a>, // extern
+            Vec<MethodQualifier<'a>>,
+            MethodPrototype<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    ConstructorDeclaration(
+        Box<(Vec<MethodQualifier<'a>>, ClassConstructorDeclaration<'a>)>,
+    ),
+    ConstructorPrototype(
+        Box<(
+            Metadata<'a>, // extern
+            Vec<MethodQualifier<'a>>,
+            ClassConstructorPrototype<'a>,
+        )>,
+    ),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ListOfArgumentsOrDefault<'a> {
+    ListOfArguments(Box<ListOfArguments<'a>>),
+    Default(Box<Metadata<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ClassConstructorDeclaration<'a>(
+    pub Metadata<'a>, // function
+    pub Option<ClassScope<'a>>,
+    pub Metadata<'a>, // new
+    pub  Option<(
+        Metadata<'a>, // (
+        Option<ClassConstructorArgList<'a>>,
+        Metadata<'a>, // )
+    )>,
+    pub Metadata<'a>, // ;
+    pub Vec<BlockItemDeclaration<'a>>,
+    pub  Option<(
+        Metadata<'a>, // super
+        Metadata<'a>, // .
+        Metadata<'a>, // new
+        Option<(
+            Metadata<'a>, // (
+            Option<ListOfArgumentsOrDefault<'a>>,
+            Metadata<'a>, // )
+        )>,
+        Metadata<'a>, // ;
+    )>,
+    pub Vec<FunctionStatementOrNull<'a>>,
+    pub Metadata<'a>, // endfunction
+    pub  Option<(
+        Metadata<'a>, // :
+        Metadata<'a>, // new
+    )>,
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ClassConstructorPrototype<'a>(
+    pub Metadata<'a>, // function
+    pub Metadata<'a>, // new
+    pub  Option<(
+        Metadata<'a>, // (
+        Option<ClassConstructorArgList<'a>>,
+        Metadata<'a>, // )
+    )>,
+    pub Metadata<'a>, // ;
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ClassConstructorArgList<'a>(
+    pub ClassConstructorArg<'a>,
+    pub  Vec<(
+        Metadata<'a>, // ,
+        ClassConstructorArg<'a>,
+    )>,
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ClassConstructorArg<'a> {
+    TfPort(Box<TfPortItem<'a>>),
+    Default(Box<Metadata<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum InterfaceClassItem<'a> {
+    Type(Box<TypeDeclaration<'a>>),
+    InterfaceClass(Box<(Vec<AttributeInstance<'a>>, InterfaceClassMethod<'a>)>),
+    LocalParameter(
+        Box<(
+            LocalParameterDeclaration<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    Parameter(
+        Box<(
+            ParameterDeclaration<'a>,
+            Metadata<'a>, // ;
+        )>,
+    ),
+    Null(Box<Metadata<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct InterfaceClassMethod<'a>(
+    pub Metadata<'a>, // pure
+    pub Metadata<'a>, // virtual
+    pub MethodPrototype<'a>,
+    pub Metadata<'a>, // ;
+);
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ClassConstraint<'a> {
+    Prototype(Box<ConstraintPrototype<'a>>),
+    Declaration(Box<ConstraintDeclaration<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum ClassItemQualifier<'a> {
+    Static(Metadata<'a>),
+    Protected(Metadata<'a>),
+    Local(Metadata<'a>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum PropertyQualifier<'a> {
+    Random(Box<RandomQualifier<'a>>),
+    ClassItem(Box<ClassItemQualifier<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum RandomQualifier<'a> {
+    Rand(Metadata<'a>),
+    Randc(Metadata<'a>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum MethodQualifier<'a> {
+    PureVirtual(
+        Box<(
+            Option<Metadata<'a>>, // pure
+            Metadata<'a>,         // virtual
+        )>,
+    ),
+    ClassItem(Box<ClassItemQualifier<'a>>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum MethodPrototype<'a> {
+    Task(Box<TaskPrototype<'a>>),
+    Function(Box<FunctionPrototype<'a>>),
+}
